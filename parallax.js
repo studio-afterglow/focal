e221687fcd6a8a65d4900cbb2c6523e47bc4755c
studio-afterglow <47209883+studio-afterglow@users.noreply.github.com>
2019-04-20 18:01:52 +0200

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -5,9 +5,9 @@ $(window).scroll(function() {
 function paralaksa(){
     var wScroll = $(window).scrollTop();
     
-    $('.part-image').css('margin-top', 6-(wScroll*0.005)+'em')
+    $('.part-image').css('margin-top', 6-(wScroll*0.008)+'em')
     
-    $('.part-right').css('margin-top', 8-(wScroll*0.009)+'em')
+    $('.part-right').css('margin-top', 8-(wScroll*0.005)+'em')
     
     $('.part-left').css('margin-top', 8-(wScroll*0.02)+'em')
     
