b5058fdeb2a6341d9234e5a7878f8de430d61d37
studio-afterglow <47209883+studio-afterglow@users.noreply.github.com>
2019-04-20 18:00:55 +0200

    Add files via upload

---
+++ parallax.js
@@ -0,0 +1,14 @@
+$(window).scroll(function() {
+    paralaksa();
+})
+
+function paralaksa(){
+    var wScroll = $(window).scrollTop();
+    
+    $('.part-image').css('margin-top', 6-(wScroll*0.005)+'em')
+    
+    $('.part-right').css('margin-top', 8-(wScroll*0.009)+'em')
+    
+    $('.part-left').css('margin-top', 8-(wScroll*0.02)+'em')
+    
+}
