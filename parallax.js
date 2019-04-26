9051e63eaf925d89763a8f91c446096d3a5ee82d
studio-afterglow <47209883+studio-afterglow@users.noreply.github.com>
2019-04-26 22:10:40 +0200

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -15,6 +15,6 @@ function paralaksa(){
     $('.part-left').css('opacity', (wScroll*0.02))
     $('.part-image').css('opacity', (wScroll*0.008))
     
-    $('.banner').css('opacity', (wScroll*0.005)-5)
+    $('.banner').css('opacity', (wScroll*0.005)-4)
     $('.opinions').css('opacity', (wScroll*0.005)-8)
 }
